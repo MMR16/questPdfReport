dd00d64da05258a90d4a62f30d71c1b754830483
MMR16 <MMR16RADY@YAHOO.COM>
2024-08-08 17:28:19 +0300

    try to make report

---
+++ Program.cs
@@ -0,0 +1,299 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
+using QuestPDFTest;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+
+    private static SvgImage Header1Image { get; } = SvgImage.FromFile("Resources/Images/Header1.svg");
+    private static SvgImage Header2Image { get; } = SvgImage.FromFile("Resources/Images/Header2.svg");
+    private static SvgImage TwitterLogo { get; } = SvgImage.FromFile("Resources/Images/TwitterLogo.svg");
+    private static SvgImage InstagramLogo { get; } = SvgImage.FromFile("Resources/Images/InstagramLogo.svg");
+    private static SvgImage SnapchatLogo { get; } = SvgImage.FromFile("Resources/Images/SnapchatLogo.svg");
+    private static SvgImage TelephoneImage { get; } = SvgImage.FromFile("Resources/Images/Telephone.svg");
+    private static SvgImage PhoneImage { get; } = SvgImage.FromFile("Resources/Images/Phone.svg");
+    private static SvgImage Location { get; } = SvgImage.FromFile("Resources/Images/Location.svg");
+
+    private static void Main(string[] args)
+    {
+
+        var document = new ReportDocument();
+        document.ShowInPreviewer();
+      //  document.GeneratePdf("report.pdf");
+
+        var Model = new JournalInvovice
+        {
+            Adress = "adress",
+            branch = "Branch",
+            CompanyName = "Company Name",
+            CreatedDate = DateOnly.FromDateTime(DateTime.Now),
+            Description = "Description kfdnasklfn sdfna ngkdsm klm",
+            FirstPhoneNum = "5415154121",
+            SecondPhoneNum = "42121515212541",
+            InvoiceNumber = "1024",
+            IsPaid = "paid",
+            Number = 10,
+            PaymentDate = DateOnly.FromDateTime(DateTime.Now),
+            PaymentMethod = "Bank",
+            Reciever = "MMR",
+            Supplier = "Ali",
+            TaxAmount = 10,
+            TaxNumber = "vat 12152",
+            Total = 100,
+            TotalAmount = 110,
+            Files = new List<JournalFileInvoice>  {
+                new JournalFileInvoice{ Name="file name",URL="new url"}
+            },
+            JournalDetails = new List<JournalDetailInvoice>
+            {
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+                new JournalDetailInvoice { Description="Journal details descriptin",ExpenseName="Expense",ExpenseNumber=10,Amount=100,TaxAmount = 10,TotalAmount=110,TaxPercentage = "10%" },
+            }
+        };
+
+        Console.WriteLine("Hello, World!");
+        //ConfigureQuestPDF();
+        //var document = Document.Create(container =>
+        //{
+
+        //    container.Page(page =>
+        //            {
+
+        //                page.MarginVertical(10);
+        //                page.MarginHorizontal(4);
+        //                page.PageColor(Colors.White);
+        //                // page.ContinuousSize(50.8f, Unit.Millimetre);
+
+        //                page.PageColor(Colors.White);
+        //                page.DefaultTextStyle(x => x.FontSize(11));
+        //                page.ContentFromRightToLeft();
+        //                page.Header().Element(ComposeHeader);
+        //                page.Content().Element(ComposeContent);
+        //            });
+        //});
+        //document.ShowInPreviewer();
+        //void ConfigureQuestPDF()
+        //{
+        //    QuestPDF.Settings.License = LicenseType.Community;
+
+        //    FontManager.RegisterFont(File.OpenRead("Fonts/Almarai-Regular.ttf"));
+        //    FontManager.RegisterFont(File.OpenRead("Fonts/LibreBarcode39Extended-Regular.ttf"));
+        //    FontManager.RegisterFont(File.OpenRead("Fonts/LibreBarcode128-Regular.ttf"));
+        //}
+
+        //void ComposeHeader(IContainer container)
+        //{
+        //    container.Row(row =>
+        //    {
+        //        row.Spacing(1);
+
+        //        row.RelativeItem().Column(column =>
+        //        {
+        //            column
+        //            .Item()
+        //            .Height(10)
+        //            .Svg(Header1Image);
+        //        });
+
+        //        row.RelativeItem().Column(column =>
+        //        {
+        //            column
+        //                .Item().PaddingLeft(20).AlignLeft().Text("سند مصروف").FontSize(12).ExtraBold();
+        //        });
+        //        row.AutoItem().Column(column =>
+        //        {
+        //            column.Item()
+        //            .Height(10)
+        //            .Svg(Header2Image);
+        //        });
+        //    });
+        //}
+        //void HorizontalLine(IContainer container)
+        //{
+        //    container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingBottom(5);
+        //}
+
+        //void ComposeContent(IContainer container)
+        //{
+
+
+        //    //container.Row(row =>
+        //    //{
+        //    //    row.RelativeItem().Column(column =>
+        //    //    {
+        //    //        column
+        //    //        .Item()
+        //    //        .Height(10)
+        //    //        .Text("dddddddddddd");
+        //    //    });
+        //    //});
+        //    container.PaddingTop(20).PaddingRight(10).PaddingLeft(10).Column(column =>
+        //  {
+        //      column.Spacing(10);
+
+
+
+        //      column.Item().AlignRight().PaddingRight(50).Text(Model.CompanyName)
+        //            .FontSize(12).ExtraBold();
+
+        //      column.Item().ShowIf(Model.SecondPhoneNum != null).Row(row =>
+        //      {
+        //          row.ConstantItem(10).Svg(TelephoneImage);
+        //          row.RelativeItem().PaddingRight(5);
+        //      });
+
+        //      column.Item().AlignRight().Text(text =>
+        //      {
+        //          text.Span("الرقم الضريبي / ").Medium();
+        //          text.Span(Model.TaxNumber).Light();
+        //      });
+
+        //      column.Item().AlignRight().ShowIf(Model.TaxAmount != null).Text(text =>
+        //      {
+        //          text.Span("رقم السجل التجاري / ").Medium();
+        //          text.Span(Model.Number.ToString()).Light();
+        //      });
+
+        //      column.Item().Row(row =>
+        //      {
+        //          row.RelativeItem().Text("أصدرت بواسطة");
+
+        //          row.RelativeItem().AlignLeft().Text(Model.Reciever);
+        //      });
+
+        //      column.Item().Row(row =>
+        //      {
+        //          row.RelativeItem().Text("رقم الكاشير");
+
+        //          row.RelativeItem().AlignLeft().Width(30).Border(1).Column(column =>
+        //          {
+        //              column.Item().AlignCenter().AlignMiddle().Text(Model.InvoiceNumber.ToString());
+        //          });
+        //      });
+
+        //      //column.Item().Row(row =>
+        //      //{
+        //      //    if (Model.Adress != null)
+        //      //    {
+        //      //        row.ConstantItem(10).Svg(Location);
+
+        //      //        row.AutoItem().PaddingRight(5).Column(column =>
+        //      //        {
+        //      //            column.Item().Text(Model.Adress);
+        //      //            //  column.Item().Text(Model.Branch.Address2);
+        //      //        });
+        //      //    }
+
+        //      //    row.RelativeItem().AlignLeft().Width(80).Column(column =>
+        //      //    {
+        //      //        column.Spacing(5);
+        //      //        column.Item().ShowIf(Model.FirstPhoneNum != null).Row(row =>
+        //      //        {
+        //      //            row.ConstantItem(10).Svg(PhoneImage);
+        //      //            row.RelativeItem().PaddingRight(5).Text(Model.FirstPhoneNum);
+        //      //        });
+
+        //      //        column.Item().ShowIf(Model.SecondPhoneNum != null).Row(row =>
+        //      //        {
+        //      //            row.ConstantItem(10).Svg(TelephoneImage);
+        //      //            row.RelativeItem().PaddingRight(5).Text(Model.SecondPhoneNum);
+        //      //        });
+
+        //      //    });
+        //      //});
+
+        //      column.Item().Element(HorizontalLine);
+
+
+        //      //column.Item().Row(row =>
+        //      //{
+        //      //    row.AutoItem().Column(column =>
+        //      //    {
+        //      //        column.Spacing(6);
+        //      //        column.Item().Text(text =>
+        //      //        {
+        //      //            text.Span("تاريخ الإصدار / ");
+        //      //            text.Span(Model.CreatedDate.ToShortDateString()).Light();
+        //      //        });
+
+        //      //        column.Item().Text(text =>
+        //      //        {
+        //      //            text.Span("وقت الإصدار / ");
+        //      //            text.Span(Model.CreatedDate.ToString()).Light();
+        //      //        });
+        //      //    });
+
+
+        //      //    row.RelativeItem().AlignLeft().Column(column =>
+        //      //    {
+        //      //        column.Spacing(6);
+        //      //        column.Item().Text("رقم الفاتورة");
+        //      //        column.Item().AlignCenter().Text(Model.Number.ToString()).FontSize(14).ExtraBold();
+        //      //    });
+        //      //});
+
+        //      //column.Item().Element(HorizontalLine);
+
+        //      //column.Item().PaddingRight(10).Text(Model.CompanyName).FontSize(16).Bold();
+
+        //      //column.Item().Element(HorizontalLine);
+
+        //      //column.Item().Element(ComposeTable);
+
+        //      //column.Item().Element(HorizontalLine);
+
+        //      //column.Item().Element(ComposeTotals);
+
+        //      //column.Item().Element(ComposePayment);
+
+
+
+
+        //      column.Item().Element(HorizontalLine);
+
+        //      //column.Item().ShowIf(Model.Barcode != null).AlignCenter().Row(row =>
+        //      //{
+        //      //    row.ConstantItem(100).Image(Model.Barcode!);
+        //      //});
+
+        //      //column.Item().ShowIf(Model.Company.ReportNotes != null).Element(ComposeComments);
+        //      //column.Item().Element(ComposeSocials);
+
+        //      column.Item().PaddingTop(5).Row(row =>
+        //      {
+        //          row.AutoItem().Text("نسخة 1");
+        //          row.RelativeItem().AlignCenter().Text("شكرا لزيارتكم").SemiBold();
+        //      });
+        //  });
+        //}
+
+
+    }
+}
+
+
+public class PrintProductsBarcodes
+{
+    public string CompanyName { get; set; }
+    public string Name { get; set; }
+    public string Barcode { get; set; }
+    public decimal Price { get; set; }
+    public string CurrencyCode { get; set; }
+}
---
+++ ReportDocument .cs
@@ -0,0 +1,125 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestPDFTest
+{
+
+    public class ReportDocument : IDocument
+    {
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+      // public DocumentSettings GetSettings() => new DocumentSettings { Size = PageSizes.A4, Margin = 20 };
+
+        public void Compose(IDocumentContainer container)
+        {
+            container
+                .Page(page =>
+                {
+                    page.Margin(20);
+
+                    page.Header().Element(ComposeHeader);
+                    page.Content().Element(ComposeContent);
+                    page.Footer().Element(ComposeFooter);
+                });
+        }
+
+        void ComposeHeader(IContainer container)
+        {
+            container.Column(column =>
+            {
+                column.Item().Row(row =>
+                {
+                    row.RelativeItem().Column(column =>
+                    {
+                        column.Item().Text("سند مصروف").FontSize(20).SemiBold();
+                        column.Item().Row(innerRow =>
+                        {
+                            innerRow.RelativeItem().Text("رقم السند").Bold();
+                            innerRow.RelativeItem().Text("24").AlignRight();
+                        });
+                        column.Item().Row(innerRow =>
+                        {
+                            innerRow.RelativeItem().Text("تاريخ الاصدار").Bold();
+                            innerRow.RelativeItem().Text("20 / 09 / 2022").AlignRight();
+                        });
+                    });
+
+                    row.RelativeItem().Column(column =>
+                    {
+                        column.Item().AlignRight().Text("شركة الشرق الأوسط المتقدم").FontSize(16).SemiBold();
+                        column.Item().AlignRight().Text("الرقم الضريبي: 310098765678654").FontSize(10);
+                        column.Item().AlignRight().Text("العنوان: جدة - حي الحمدانية").FontSize(10);
+                    });
+
+                    //row.ConstantItem(50).Image("logo.png"); // Placeholder for company logo
+                });
+
+                column.Item().PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Grey.Lighten1);
+            });
+        }
+
+        void ComposeContent(IContainer container)
+        {
+            container.PaddingVertical(10).Column(column =>
+            {
+                column.Item().Table(table =>
+                {
+                    table.ColumnsDefinition(columns =>
+                    {
+                        columns.RelativeColumn(2);
+                        columns.RelativeColumn(3);
+                        columns.RelativeColumn(1);
+                        columns.RelativeColumn(1);
+                        columns.RelativeColumn(1);
+                    });
+
+                    table.Header(header =>
+                    {
+                        header.Cell().Text("المصروف").AlignCenter().Bold();
+                        header.Cell().Text("الوصف").AlignCenter().Bold();
+                        header.Cell().Text("المبلغ").AlignCenter().Bold();
+                        header.Cell().Text("نسبة الضريبة").AlignCenter().Bold();
+                        header.Cell().Text("قيمة الضريبة").AlignCenter().Bold();
+                    });
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        table.Cell().Text("مصاريف الكهرباء").AlignCenter();
+                        table.Cell().Text("فاتورة كهرباء شهر أكتوبر لفرع الحمدانية").AlignCenter();
+                        table.Cell().Text("100.00 ر.س").AlignCenter();
+                        table.Cell().Text("15%").AlignCenter();
+                        table.Cell().Text("150.00 ر.س").AlignCenter();
+                    }
+
+                    table.Footer(footer =>
+                    {
+                        footer.Cell().Element(CellStyle).Text("إجمالي مبلغ المصروف");
+                        footer.Cell().Element(CellStyle).Text("إجمالي ضريبة القيمة المضافة");
+                        footer.Cell().Element(CellStyle).Text("الإجمالي الكلي");
+                        footer.Cell().ColumnSpan(2).Element(CellStyle).Text("1,150.00 ر.س").AlignCenter().Bold();
+                    });
+                });
+            });
+        }
+
+        void ComposeFooter(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row.RelativeItem().Text("ملاحظة: هذه فاتورة تجريبية لاستخدام QuestPDF").FontSize(10);
+                row.ConstantItem(100).Height(50).Placeholder();
+            });
+        }
+
+        IContainer CellStyle(IContainer container)
+        {
+            return container.Border(1).BorderColor(Colors.Grey.Lighten2).Padding(5).AlignCenter().AlignMiddle();
+        }
+    }
+}
+
