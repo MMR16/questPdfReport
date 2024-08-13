34493ed283685be3592905bcaa1a4b17b3ff2f96
MMR16 <MMR16RADY@YAHOO.COM>
2024-08-13 15:51:57 +0300

    Add API Files

---
+++ QuestPdfAPI/Controllers/QuestPdfController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using QuestPDF.Fluent;
+using QuestPDF.Previewer;
+using QuestPDFTest;
+using System.Net;
+
+namespace QuestPdfAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestPdfController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GenerateReport()
+        {
+            var document = new ReportDocument();
+            // Generate the PDF document in memory
+            var pdfBytes = document.GeneratePdf();
+
+            // Return the PDF as a file
+            return File(pdfBytes, "application/pdf", "SampleReport.pdf");
+        }
+    }
+}
